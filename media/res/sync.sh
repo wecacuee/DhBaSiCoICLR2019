22c36e230ac64cb9b00dbde03cc4efd7cc689a56
Vikas Dhiman <dhiman@umich.edu>
2018-09-17 22:21:48 -0400

    added more experiments

---
+++ media/res/sync.sh
@@ -0,0 +1,1 @@
+rsync -rv --include='*/' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/{d047a03,3a90344}-FetchReach-v1-* ./
