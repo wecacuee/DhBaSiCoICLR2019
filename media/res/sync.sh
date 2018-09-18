818a0a328c0bce21335b5f8fcc495b181aafe7de
Vikas Dhiman <dhiman@umich.edu>
2018-09-18 08:42:09 -0400

    FetchPush tears apart the difference between working and non-working

--- media/res/sync.sh
+++ media/res/sync.sh
@@ -1,1 +1,2 @@
 rsync -rv --include='*/' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/{d047a03,3a90344}-FetchReach-v1-* ./
+rsync -rv --include='*/' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/ea0e35b-FetchPush-v1-* ./
