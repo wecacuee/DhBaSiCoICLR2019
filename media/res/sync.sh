b65c50f8f2b96a9196a0cffad0c202a521b97802
Vikas Dhiman <dhiman@umich.edu>
2018-09-23 15:41:10 -0400

    more experiments

--- media/res/sync.sh
+++ media/res/sync.sh
@@ -4,6 +4,8 @@ rsync -rav --include='*/' --include='params.json' --include='progress.csv' --inc
 rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/f0d4cfa-FetchReach-v1-* ./
 rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/f84daa7-FetchPush-v1-* ./
 rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/3f1eafe-FetchPush-v1-* ./
-rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/38f4625-Fetch*-v1-*future ./
+# The BIG experiment on all environments
+rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/38f4625-*Fetch*-v1-*future ./
+rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/38f4625-*Hand*-v0-*future ./
 # Experiment to compare weighted loss terms (excluded the step term)
 rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/04a8fc6-*-FetchSlide-v1-*future ./
