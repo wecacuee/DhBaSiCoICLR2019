f1a29734ee10048292966f98e576f9d718e441f6
Vikas Dhiman <dhiman@umich.edu>
2018-09-23 21:55:22 -0400

    more experiments

--- media/res/sync.sh
+++ media/res/sync.sh
@@ -11,3 +11,7 @@ rsync -rav --include='*/' --include='params.json' --include='progress.csv' --inc
 rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/04a8fc6-*-FetchSlide-v1-*future ./
 # Experiment to compare effect of path reward
 rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/3d07a6e-FetchReachPR-v1-*-future ./
+# Experiemnt to avoid re-computation of rewards for each goal.
+rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/245b3c4-*-FetchReach*-v1-*-future-her_fwrl_path_reward ./
+# Effect of middle vs uniformly random sampling for chosing intermediate state
+rsync -rav --include='*/' --include='params.json' --include='progress.csv' --include='*test/*.pdf' --include='train/*loss*.pdf' --exclude='*' /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/d249d2d-c9bfa98b-FetchPush-v1-fwrl-future /z/home/dhiman/mid/floyd-warshall-rl/openai-baselines/her/245b3c4-e5f0dd20-FetchPush-v1-fwrl-future- ./
